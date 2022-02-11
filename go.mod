f6a82b14894d81780adf9dafb0dd8a1e9f4bbaf8
Suryansh Kumar <201951158@iiitvadodara.ac.in>
2022-02-11 17:46:21 +0530

    added write , read function for databse

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dragno99/go-database
 
 go 1.17
+
+require github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25 // indirect
---
+++ main.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/jcelliott/lumber"
+)
+
+const Version = "1.0.0"
+
+type (
+	Logger interface {
+		Fatal(string, ...interface{})
+		Error(string, ...interface{})
+		Warn(string, ...interface{})
+		Debug(string, ...interface{})
+		Trace(string, ...interface{})
+		Info(string, ...interface{})
+	}
+	Driver struct {
+		mutex   sync.Mutex
+		mutexes map[string]*sync.Mutex
+		dir     string
+		log     Logger
+	}
+)
+
+type Options struct {
+	Logger
+}
+
+func New(dir string, options *Options) (*Driver, error) {
+	dir = filepath.Clean(dir)
+	opts := Options{}
+	if options != nil {
+		opts = *options
+	}
+	if opts.Logger == nil {
+		opts.Logger = lumber.NewConsoleLogger((lumber.INFO))
+	}
+
+	driver := Driver{
+		dir:     dir,
+		mutexes: make(map[string]*sync.Mutex),
+		log:     opts.Logger,
+	}
+	if _, err := os.Stat(dir); err == nil {
+		opts.Logger.Debug("Using '%s' (database already exixts)\n", dir)
+		return &driver, nil
+	}
+	opts.Logger.Debug("Creating the databse at '%s'...\n", dir)
+	return &driver, os.MkdirAll(dir, 0755)
+}
+
+func stat(path string) (fi os.FileInfo, err error) {
+	if fi, err = os.Stat(path); os.IsNotExist(err) {
+		fi, err = os.Stat(path + ".json")
+	}
+	return
+}
+
+func (d *Driver) Write(collection string, resource string, value interface{}) error {
+	if collection == "" {
+		return fmt.Errorf("Missing collection - no place to save records")
+	}
+	if resource == "" {
+		return fmt.Errorf("Missing resource - unable to save records (no name)")
+	}
+	mutex := d.getOrCreateMutex(collection)
+	mutex.Lock()
+	defer mutex.Unlock()
+	dir := filepath.Join(d.dir, collection)
+	finalPath := filepath.Join(dir, resource+".json")
+	tempPath := finalPath + ".tmp"
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+	b, err := json.MarshalIndent(value, "", "\t")
+	if err != nil {
+		return err
+	}
+	b = append(b, byte('\n'))
+
+	if err := ioutil.WriteFile(tempPath, b, 0644); err != nil {
+		return err
+	}
+
+	return os.Rename(tempPath, finalPath)
+}
+
+func (d *Driver) Read(collection string, resource string, value interface{}) error {
+	if collection == "" {
+		return fmt.Errorf("Missing collection - no place to save record")
+	}
+	if resource == "" {
+		return fmt.Errorf("Missing resource - unable to save record (no name)")
+	}
+
+	record := filepath.Join(d.dir, collection, resource)
+
+	if _, err := stat(record); err != nil {
+		return err
+	}
+	b, err := ioutil.ReadFile(record + ".json")
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, &value)
+}
+
+func (d *Driver) ReadAll() {
+
+}
+
+func (d *Driver) Delete() error {
+
+}
+
+func (d *Driver) getOrCreateMutex(collection string) *sync.Mutex {
+	d.mutex.Lock()
+	defer d.mutex.Unlock()
+	m, ok := d.mutexes[collection]
+	if !ok {
+		m = &sync.Mutex{}
+		d.mutexes[collection] = m
+	}
+	return m
+}
+
+type Address struct {
+	City    string
+	State   string
+	Country string
+	Pincode json.Number
+}
+
+type User struct {
+	Name    string
+	Age     json.Number
+	Contact string
+	Company string
+	Address Address
+}
+
+func main() {
+
+	dir := "./"
+
+	db, err := New(dir, nil)
+	if err != nil {
+		fmt.Println("Error ", err)
+	}
+
+	employees := []User{
+		{"Ravi", "25", "2929292", "My tech", Address{"banglore", "karnatak", "india", "433221"}},
+		{"Rahul", "24", "2929292", "Gojek", Address{"delhi", "karnatak", "india", "433221"}},
+		{"Shivam", "23", "2929292", "Apple", Address{"chennai", "karnatak", "india", "433221"}},
+		{"Kartik", "21", "2929292", "URI", Address{"indore", "karnatak", "india", "433221"}},
+		{"Rohan", "34", "2929292", "FinTech", Address{"banglore", "karnatak", "india", "433221"}},
+		{"Nitin", "21", "2929292", "War", Address{"pune", "karnatak", "india", "433221"}},
+	}
+
+	// use of write one function
+	for _, value := range employees {
+		db.write("users", value.Name, User{
+			Name:    value.Name,
+			Age:     value.Age,
+			Contact: value.Contact,
+			Company: value.Company,
+			Address: value.Address,
+		})
+	}
+
+	records, err := db.ReadAll("users")
+	if err != nil {
+		fmt.Println("Error ", err)
+	}
+
+	fmt.Println(records)
+
+	allusers := []User{}
+
+	for _, x := range records {
+		employeesFound := User{}
+		if err := json.Unmarshal([]byte(x), &employeesFound); err != nil {
+			fmt.Println("Error ", err)
+		}
+		allusers = append(allusers, employeesFound)
+	}
+	fmt.Println(allusers)
+
+	// if err := db.Delete("user", "Jhon"); err != nil {
+	// 	fmt.Println("Error ", err)
+	// }
+
+	// if err := db.Delete("user", ""); err != nil {
+	// 	fmt.Println("Error ", err)
+	// }
+
+}
